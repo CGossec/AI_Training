017a0dacb14ca1567fc8001f866911d50cef806c
Celian Gossec <celian.gossec@epita.fr>
2018-12-13 09:13:43 +0100

    Update: completed tutorial on creating an easy neural network (https://machinelearningmastery.com/tutorial-first-neural-network-python-keras)

--- keras_first_NN/easy_neural.py
+++ keras_first_NN/easy_neural.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
 import keras
-import numpy as np
+import numpy
 numpy.random.seed(0)
+# load pima indians dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = keras.Sequential()
+model.add(keras.layers.Dense(12, input_dim=8, activation='relu'))
+model.add(keras.layers.Dense(8, activation='relu'))
+model.add(keras.layers.Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
